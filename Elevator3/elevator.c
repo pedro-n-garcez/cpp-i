8d1d7c96930a3eed185a0bd3c504f83aeb21ee5f
Pedro <pedro.garcez@loop.colum.edu>
2020-03-03 21:05:24 -0600

    final version

--- Elevator3/elevator.c
+++ Elevator3/elevator.c
@@ -3,7 +3,9 @@
 #include<stdbool.h>
 
 int currentFloor = 0;
-int whereTo = 0;
+int chosenAmount = 0;
+bool swap = true;
+int temp = 0;
 
 typedef struct Node{
 	struct Node *prev;
@@ -30,17 +32,6 @@ void Append(int new_data){
 	new_node->prev = last;
 }
 
-bool search(int user_input){
-	node_t *last = head;
-	while(last != NULL){
-		if(last->data == user_input){
-			return true;
-		}
-		last = last->next;
-	}
-	return false;
-}
-
 node_t* get_node(int node_index) 
 {
 	int counter = 0;
@@ -56,36 +47,84 @@ node_t* get_node(int node_index)
 
 void elevator()
 {
-    printf("We're on floor %d, the top floor is 15. Where to? ",currentFloor);
-	scanf("%d",&whereTo);
-	
-	if (search(whereTo) == true){
+    printf("We're on floor %d, the top floor is 15. How many floors do you want to visit?\n",currentFloor);
+    scanf("%d",&chosenAmount);
+   
+    int whereTo[chosenAmount];
+    size_t wt_len = sizeof(whereTo)/sizeof(whereTo[0]);
+   
+    for (int i = 0; i < wt_len; i++)
+    {
+        printf("Floor number: ");
+        scanf("%d",&whereTo[i]);
+    }
+    
+    //FIND IF IN LIST
+    bool invalid_element;
+    bool invalid_input = false;
+    for(int i = 0; i<wt_len;i++){
+    		invalid_element = true;
+    		node_t *last = head;
+    		while(last != NULL){
+				if(last->data == whereTo[i]){
+					invalid_element = false;
+				}
+			last = last->next;
+		}
+		if (invalid_element == true){
+			invalid_input = true;
+		}		
+	}
+    
+	if (invalid_input == false)
+	{
+		//sorting
+        while (swap == true)
+        {
+            swap = false;
+            for (int i = 0; i < wt_len - 1; i++)
+            {
+                if (whereTo[i] > whereTo[i+1])
+                {
+                    temp = whereTo[i];
+                    whereTo[i] = whereTo[i+1];
+                    whereTo[i+1] = temp;
+                    swap = true;
+                }
+            }
+        }
+        
+        printf("Mind the gate...");
 		node_t* last = get_node(currentFloor);
-		if(whereTo>currentFloor)
+		for (int i = 0; i < wt_len; i++)
 		{
-	    	while(last->data != whereTo) {
-		    	printf("%d\n",last->data);
+			if(currentFloor <= whereTo[i])
+			{
+	    	while(last->data != whereTo[i]) {
+	    		printf("%d\n",last->data);	    			
 		    	currentFloor++;
 		    	last = last->next;
 	    	} printf("DING! You're on floor %d.\n",currentFloor);
-		} else if (whereTo<currentFloor)
-		{
-	    	while(last->data != currentFloor) {
-		    	last = last->next;
-	    	}
-	    	while(last->data != whereTo) {
-		    	printf("%d\n",last->data);
-		    	currentFloor--;
-		    	last = last->prev;
-	    	} printf("DING! You're on floor %d.\n",currentFloor);
-		} printf("Want to go to another floor? y/n ");
+			} else if (currentFloor >= whereTo[i])
+			{
+	    		while(last->data != currentFloor) {
+		    		last = last->next;
+	    		}
+	    		while(last->data != whereTo[i]) {
+	    			printf("%d\n",last->data);	    			
+		    		currentFloor--;
+		    		last = last->prev;
+	    		} printf("DING! You're on floor %d.\n",currentFloor);
+			}
+		}
+		printf("Want to go to another floor? y/n ");
 		char choice;
 		scanf("%s",&choice);
 		if (choice == 'y' || choice == 'Y'){
 	    	elevator();
 		}		
-	} else {
-		printf("That's not a valid floor, mate. Try again.\n");
+	} else if (invalid_input == true) {
+		printf("You input an invalid floor, mate. Try again.\n");
 		elevator();
 	}
 }
